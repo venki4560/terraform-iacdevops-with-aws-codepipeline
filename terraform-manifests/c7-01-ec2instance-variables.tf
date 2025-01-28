d29943243130b539ea8efd8fd2f14d1a1a9d3e65
Venkatesh Appala <venkateshappala@Venkateshs-MacBook-Pro.local>
2025-01-28 13:00:19 +0530

    first commit

--- terraform-manifests/c7-01-ec2instance-variables.tf
+++ terraform-manifests/c7-01-ec2instance-variables.tf
@@ -5,14 +5,14 @@
 variable "instance_type" {
   description = "EC2 Instance Type"
   type = string
-  default = "t3.micro"  
+  default = "t2.micro"  
 }
 
 # AWS EC2 Instance Key Pair
 variable "instance_keypair" {
   description = "AWS EC2 Key pair that need to be associated with EC2 Instance"
   type = string
-  default = "terraform-key"
+  default = "terraform"
 }
 
 # AWS EC2 Private Instance Count
